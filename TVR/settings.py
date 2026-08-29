eda816e4d35251b90f75092835206eca546b1140
vetriselvi898983 <vetriselvi898983@gmail.com>
2026-08-29 16:32:53 +0530

    Update SECRET_KEY to use environment variable

--- TVR/settings.py
+++ TVR/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ie2t1nc)+h+^td=3r+85@(m#4k@n1i@bi@b0rth8$&&h1(0zb_'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
