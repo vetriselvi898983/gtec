cefd44044c650c84aba8a5c2ae6d214e5e267ed4
vetriselvi898983 <vetriselvi898983@gmail.com>
2026-08-29 16:30:37 +0530

    Update database settings to use environment variables

--- TVR/settings.py
+++ TVR/settings.py
@@ -79,14 +79,13 @@ WSGI_APPLICATION = 'TVR.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'gtec',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'NAME': os.environ.get('MYSQLDATABASE'),
+        'USER': os.environ.get('MYSQLUSER'),
+        'PASSWORD': os.environ.get('MYSQLPASSWORD'),
+        'HOST': os.environ.get('MYSQLHOST'),
+        'PORT': os.environ.get('MYSQLPORT', '3306'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
