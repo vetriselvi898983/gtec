1c4b3e389e27ed80fb7d231d14c0c613034453ee
vetriselvi898983 <vetriselvi898983@gmail.com>
2026-08-31 11:55:13 +0530

    Update settings.py

--- TVR/settings.py
+++ TVR/settings.py
@@ -132,7 +132,7 @@ STATICFILES_DIRS = [
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'frontend/static/media')
+MEDIA_ROOT = '/app/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
